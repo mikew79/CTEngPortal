6c0aa0fb30de30b0d0dd5e52cb8ea1de3e4cd1cb
mikew79 <46069088+mikew79@users.noreply.github.com>
2018-12-22 13:27:59 +0000

    Initial Commit
    Initial Commit

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: 'login.component.html'
+})
+export class LoginComponent {
+
+  LoginClicked() {
+    
+  }
+
+  ForgotClicked() {
+
+  }
+
+  RequestClicked() {
+
+  }
+
+ }
